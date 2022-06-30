09846743e7ce5f089af60863797f990c1acd3e2e
A139785743 <elevea139785743@edenschool.fr>
2022-06-30 15:36:24 +0200

    Evaluation finale

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // les let et les const
 let platsDiv = document.getElementById("cartePlats");
+let darkMode = document.querySelector("header button");
+let header = document.querySelector("header");
+let main = document.querySelector("main");
+let footer = document.querySelector("footer");
+let body = document.querySelector("body");
 const plat = [
     {
     imgSrc: "assets/img/img-01.jpg",
@@ -36,3 +41,16 @@ for (const element of plat){
     article.appendChild(document.createElement("h3")).textContent = element.title;
     article.appendChild(document.createElement("p")).textContent = element.text;
 }
+
+function darker() {
+    header.style.display = "none";
+    main.style.display = "none";
+    footer.style.display = "none";
+    body.style.backgroundColor = "#4F5D75";
+    let p = document.createElement("p");
+    p.innerText = "C'est une blague ... rechargez la page"
+    p.classList.add("darkmode");
+    body.appendChild(p);
+
+}
+darkMode.addEventListener("click", darker)
