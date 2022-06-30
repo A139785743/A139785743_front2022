76182afde19713ee216cb311b0562a60bc94976f
A139785743 <elevea139785743@edenschool.fr>
2022-06-30 11:22:38 +0200

    Evaluatuion finale

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ const plat = [
         title: "un super titre",
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis culpa dignissimos dolorum id illo neque pariatur qui rerum sit. Ducimus eveniet exercitationem facere illo necessitatibus provident quos vero voluptates! Praesentium!"
     },
-]
+];
 for (const element of plat){
     let article = document.createElement("article");
     platsDiv.appendChild(article);
